7ba4786b09f51ea7a7a5423c2ee70d826409650f
chaveamin <aminchave@gmail.com>
2025-03-13 09:00:43 +0330

    client notif

--- src/app/extensions/client-notifications/page.js
+++ src/app/extensions/client-notifications/page.js
@@ -1,8 +1,11 @@
+import CTA from '@/components/CTA'
 import Features from '@/components/ext/client-notif/Features'
 import Hero from '@/components/ext/client-notif/Hero'
 import MediaManager from '@/components/ext/client-notif/MediaManager'
 import Pricing from '@/components/ext/client-notif/Pricing'
 import Slider from '@/components/ext/client-notif/Slider'
+import UseCase from '@/components/ext/client-notif/UseCase'
+import FAQ from '@/components/ext/email-template/FAQ'
 
 export const metadata = {
   title: 'افزونه اعلان مشتری برای قالب ناحیه کاربری لاگوم',
@@ -24,7 +27,10 @@ export default function page() {
       <Slider />
       <Features />
       <MediaManager />
+      <UseCase />
       <Pricing />
+      <FAQ />
+      <CTA />
     </>
   )
 }
